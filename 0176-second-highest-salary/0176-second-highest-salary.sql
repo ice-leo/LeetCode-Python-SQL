eb42e3f7308c0e89b583de3a6783e4a3a631918a
Isaiah Mariano <143186171+ice-leo@users.noreply.github.com>
2026-04-11 14:22:38 +0800

    Time: 213 ms (33.47%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+WITH ranked AS (
+        SELECT
+        salary,
+        DENSE_RANK() OVER(ORDER BY salary DESC) AS rank
+    FROM Employee
+    GROUP BY salary
+)
+SELECT
+    MAX(salary) AS SecondHighestSalary -- max of nothing returns null
+FROM ranked
+WHERE rank = 2
+
+
