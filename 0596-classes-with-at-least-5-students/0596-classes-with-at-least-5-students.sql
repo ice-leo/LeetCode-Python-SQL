dce4b35a41ad94cb92e8b06b54a8052261099d56
Isaiah Mariano <143186171+ice-leo@users.noreply.github.com>
2026-04-11 14:27:54 +0800

    Time: 268 ms (92.01%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    class
+FROM (
+    SELECT
+        class,
+        COUNT(student)
+    FROM Courses
+    GROUP BY class
+    HAVING COUNT(student) >= 5
+)
